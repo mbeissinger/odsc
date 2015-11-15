b9e78ef2d36e8389fb50aebe4830a3f79aaab51a
mbeissinger <mbeissinger@gmail.com>
2015-11-15 02:07:59 -0800

    mlp with opendeep

--- mlp_mnist.py
+++ mlp_mnist.py
@@ -33,18 +33,7 @@ if __name__ == '__main__':
     # layer's inputs.
     mlp.add(Noise, noise='dropout', noise_level=0.5)
     # add our classification layer
-    lenet.add(Conv2D, n_filters=50, filter_size=(5, 5), border_mode='full', activation='relu')
-    lenet.add(Pool2D, size=(2, 2))
-    lenet.add(Noise, noise='dropout', noise_level=0.5)
-    # reshape convolution output to be 2D to feed into fully-connected layers
-    # Prototype container keeps its layers in the `models` attribute list: grab the latest model output
-    dense_input = lenet.models[-1].get_outputs().flatten(2)
-    dense_in_shape = lenet.models[-1].output_size[:1] + (np.prod(lenet.models[-1].output_size[1:]), )
-    # now make the dense (fully-connected) layers!
-    lenet.add(Dense(inputs=(dense_in_shape, dense_input), outputs=500, activation='tanh'))
-    lenet.add(Noise, noise='dropout', noise_level=0.5)
-    # softmax classification layer!
-    lenet.add(Softmax, outputs=10, out_as_probs=False)
+    mlp.add(Softmax, outputs=10, out_as_probs=False)
 
     ################
     # Optimization #
@@ -53,24 +42,18 @@ if __name__ == '__main__':
     # first need a target variable
     labels = T.lvector('ys')
     # negative log-likelihood for classification cost
-    loss = Neg_LL(inputs=lenet.models[-1].p_y_given_x, targets=labels, one_hot=False)
+    loss = Neg_LL(inputs=mlp.models[-1].p_y_given_x, targets=labels, one_hot=False)
     # make a monitor to view average accuracy per batch
     accuracy = Monitor(name='Accuracy',
-                       expression=1-(T.mean(T.neq(lenet.models[-1].y_pred, labels))),
+                       expression=1-(T.mean(T.neq(mlp.models[-1].y_pred, labels))),
                        valid=True, test=True)
 
-    # Now grab our MNIST dataset. The version given here has each image as a single 784-dimensional vector.
-    # because convolutions work over 2d, let's reshape our data into the (28,28) images they originally were
-    # (only one channel because they are black/white images not rgb)
+    # Now grab our MNIST dataset.
     mnist = MNIST()
-    process_image = lambda img: np.reshape(img, (1, 28, 28))
-    mnist.train_inputs = ModifyStream(mnist.train_inputs, process_image)
-    mnist.valid_inputs = ModifyStream(mnist.valid_inputs, process_image)
-    mnist.test_inputs = ModifyStream(mnist.test_inputs, process_image)
 
     # finally define our optimizer and train the model!
     optimizer = AdaDelta(
-        model=lenet,
+        model=mlp,
         dataset=mnist,
         loss=loss,
         epochs=10,
